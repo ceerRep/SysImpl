ab5dd35d1ae6fc5c01774cc276dd4e181c276353
ceerrep <ceerrep@gmail.com>
2022-02-10 22:38:59 +0800

    add llvm compile-rt

--- inc/dlfcn.h
+++ inc/dlfcn.h
@@ -6,7 +6,19 @@ typedef struct
 	void *dli_saddr;
 } Dl_info;
 
+extern "C"
+{
+
 int dladdr(void *, Dl_info *)
 {
 	return 0;
+}
+
+int dl_iterate_phdr(
+        int (*callback) (struct dl_phdr_info *info, size_t size, void *data),
+        void *data)
+{
+    return -1;
+}
+
 }
